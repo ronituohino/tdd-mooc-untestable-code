1df3c90c5ce3d2b8d92916b056b11989b0a90989
Roni Tuohino <rt@ronituohino.com>
2026-03-19 17:01:50 +0200

    Add more tests to ensure test db functionality

--- test/testable4.test.mjs
+++ test/testable4.test.mjs
@@ -42,10 +42,16 @@ describe("Testable 4: enterprise application", () => {
     expect(rows[0].result).to.equal(2);
     expect(postgres.db.options.database).to.equal("untestable");
   });
-
   test("test database contains seed data", async () => {
     const { rows } = await postgres.db.query(`select password_hash as hash from users where user_id=$1`, [1]);
     expect(rows[0].hash.length).to.be.greaterThan(0);
+  });
+  test("test data does not persist across tests 1", async () => {
+    await postgres.db.query("insert into users (user_id, password_hash) values (3, 'qwe123')");
+  });
+  test("test data does not persist across tests 2", async () => {
+    const { result } = await postgres.db.query("select password_hash from users where user_id = 3");
+    expect(result).to.equal(undefined);
   });
 
   test("DAO can get a user by id", async () => {
@@ -56,8 +62,16 @@ describe("Testable 4: enterprise application", () => {
     const result = await userDao.getById(4);
     expect(result).to.equal(null);
   });
-  test.skip("DAO can use save to create a new user with id and password", () => {});
-  test.skip("DAO can use save to update an existing user's password with the same id", () => {});
+  test("DAO can use save to create a new user with id and password", async () => {
+    await userDao.save({ userId: 4, passwordHash: "112233" });
+    const result = await userDao.getById(4);
+    expect(result.passwordHash).to.equal("112233");
+  });
+  test("DAO can use save to update an existing user's password with the same id", async () => {
+    await userDao.save({ userId: 1, passwordHash: "newhashfor1" });
+    const result = await userDao.getById(1);
+    expect(result.passwordHash).to.equal("newhashfor1");
+  });
 
   test.skip("password service can change a user password when id exists and old password matches", () => {});
   test.skip("password service will throw an error if old password does not match", () => {});
