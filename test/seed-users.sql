a32a7bee8251fe800c00e89eee54bf3e506d618e
Roni Tuohino <rt@ronituohino.com>
2026-03-19 16:49:50 +0200

    Add test db seeding

--- test/seed-users.sql
+++ test/seed-users.sql
@@ -1,4 +1,4 @@
-insert into users (password_hash) values (
-  '$argon2id$v=19$m=19456,t=2,p=1$896omGJSjsS+c5wabGATLg$hYyW8RemrZZQtqOu4osMwLm/mFQQtiv4zKSDDpQROVU', -- 123qwe123
-  '$argon2id$v=19$m=19456,t=2,p=1$7ps8lYoN6B/EIsKKeDjIrg$6Lry0SSVqjqjuwv/AVPH2pongMdqNDNXjymwuZSR8Jg', -- 12345
-);
+insert into users (user_id, password_hash) values 
+  (1, '$argon2id$v=19$m=19456,t=2,p=1$896omGJSjsS+c5wabGATLg$hYyW8RemrZZQtqOu4osMwLm/mFQQtiv4zKSDDpQROVU'), -- 123qwe123
+  (2, '$argon2id$v=19$m=19456,t=2,p=1$7ps8lYoN6B/EIsKKeDjIrg$6Lry0SSVqjqjuwv/AVPH2pongMdqNDNXjymwuZSR8Jg');  -- 12345
+      
--- test/testable4.test.mjs
+++ test/testable4.test.mjs
@@ -1,26 +1,46 @@
 import { afterEach, beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
 import { PasswordService, PostgresUserDao, PostgresDB } from "../src/testable4.mjs";
+import { readFile } from "node:fs/promises";
 
 describe("Testable 4: enterprise application", () => {
-  let db;
+  let postgres;
   let userDao;
   let passwordService;
-  beforeEach(() => {
-    db = new PostgresDB("untestable", "localhost", "untestable", "secret", 5432);
-    userDao = new PostgresUserDao(db);
+
+  const seedDatabase = async (postgres) => {
+    const addUsersSql = await readFile("./test/seed-users.sql", { encoding: "utf8" });
+    await postgres.db.query(addUsersSql);
+  };
+
+  const undoSeedDatabase = async (postgres) => {
+    const truncateUsersSql = await readFile("./test/seed-undo-users.sql", { encoding: "utf8" });
+    await postgres.db.query(truncateUsersSql);
+  };
+
+  beforeEach(async () => {
+    postgres = new PostgresDB("untestable", "localhost", "untestable", "secret", 5432);
+
+    // fail safe to prevent anything being done if not test database
+    if (postgres.db.options.database !== "untestable") {
+      postgres = undefined;
+    }
+
+    userDao = new PostgresUserDao(postgres);
     passwordService = new PasswordService(userDao);
+    await seedDatabase(postgres);
   });
 
-  afterEach(() => {
-    db.close();
+  afterEach(async () => {
+    await undoSeedDatabase(postgres);
+    postgres.close();
   });
 
   test("test database is up", async () => {
-    const { rows } = await db.query(`select 1+1`);
-    console.log(rows);
+    const { rows } = await postgres.db.query(`select 1+1 as result`);
 
-    // expect 1+1 = 2
-    // expect db name == test
+    expect(rows[0].result).to.equal(2);
+    expect(postgres.db.options.database).to.equal("untestable");
   });
 
   test.skip("test database contains seed data", () => {});
